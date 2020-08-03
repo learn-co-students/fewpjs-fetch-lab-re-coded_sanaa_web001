9304c0b55eddd879ce25afdcd272b4c6cff0b1f2
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-03 10:07:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function fetchBooks() {
+return  fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json())
+    .then(json => renderBooks(json));
 
 }
 
@@ -6,7 +9,7 @@ function renderBooks(books) {
   const main = document.querySelector('main')
   books.forEach(book => {
     const h2 = document.createElement('h2')
-    h2.innerHTML = book.name
+    h2.innerHTML = book.name;
     main.appendChild(h2)
   })
 }
